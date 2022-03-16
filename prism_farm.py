4d55ffd9963385f5d9329d0bd712d934ab982b96
Jimmy Le <le.jimmy91@gmail.com>
2022-03-15 22:24:55 -0500

    New calcs

--- prism_farm.py
+++ prism_farm.py
@@ -1,7 +1,9 @@
-import math
+import base64
+import json
 import requests
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 @st.cache(show_spinner=False)
@@ -79,16 +81,107 @@ def get_staking_yield(luna_price, staked_luna):
     return staking_yield
 
 
+def dict_to_b64(data: dict) -> str:
+    """Converts dict to ASCII-encoded base64 encoded string."""
+    return base64.b64encode(bytes(json.dumps(data), "ascii")).decode()
+
+
+# query xPRISM balance in AMPS vault
+@st.cache(show_spinner=False)
+def get_amps_vault_xprism():
+
+    query_message = dict_to_b64(
+        {"balance": {"address": "terra1pa4amk66q8punljptzmmftf6ylq3ezyzx6kl9m"}}
+    )
+
+    response = requests.get(
+        f"https://lcd.terra.dev/terra/wasm/v1beta1/contracts/terra1042wzrwg2uk6jqxjm34ysqquyr9esdgm5qyswz/store?query_msg={query_message}",
+    ).json()
+
+    xprism_balance = float(response["query_result"]["balance"]) / 1e6
+
+    return xprism_balance
+
+
+# query user's pledged pledged xPRISM
+@st.cache(show_spinner=False)
+def get_user_amps(user_address):
+
+    query_message = dict_to_b64({"get_boost": {"user": user_address}})
+
+    response = requests.get(
+        f"https://lcd.terra.dev/terra/wasm/v1beta1/contracts/terra1pa4amk66q8punljptzmmftf6ylq3ezyzx6kl9m/store?query_msg={query_message}",
+    ).json()
+
+    user_xprism = float(response["query_result"]["amt_bonded"]) / 1e6
+    user_amps = float(response["query_result"]["total_boost"]) / 1e6
+
+    return user_xprism, user_amps
+
+
+# query user's AMPS amount
+@st.cache(show_spinner=False)
+def get_user_prism_farm(user_address):
+
+    query_message = dict_to_b64({"reward_info": {"staker_addr": user_address}})
+
+    response = requests.get(
+        f"https://lcd.terra.dev/terra/wasm/v1beta1/contracts/terra1ns5nsvtdxu53dwdthy3yxs6x3w2hf3fclhzllc/store?query_msg={query_message}",
+    ).json()
+
+    user_yluna = float(response["query_result"]["bond_amount"]) / 1e6
+    user_weight = float(response["query_result"]["boost_weight"]) / 1e6
+
+    return user_yluna, user_weight
+
+
+# query amount of yLUNA in PRISM Farm
+@st.cache(show_spinner=False)
+def get_yluna_staked():
+
+    query_message = dict_to_b64(
+        {"reward_info": {"staker_addr": "terra1ns5nsvtdxu53dwdthy3yxs6x3w2hf3fclhzllc"}}
+    )
+
+    response = requests.get(
+        f"https://lcd.terra.dev/terra/wasm/v1beta1/contracts/terra1p7jp8vlt57cf8qwazjg58qngwvarmszsamzaru/store?query_msg={query_message}",
+    ).json()
+
+    yluna_staked = float(response["query_result"]["staked_amount"]) / 1e6
+
+    return yluna_staked
+
+
+# query user's AMPS weight
+@st.cache(show_spinner=False)
+def get_total_boost_weight():
+
+    query_message = dict_to_b64({"distribution_status": {}})
+
+    response = requests.get(
+        f"https://lcd.terra.dev/terra/wasm/v1beta1/contracts/terra1ns5nsvtdxu53dwdthy3yxs6x3w2hf3fclhzllc/store?query_msg={query_message}",
+    ).json()
+
+    total_boost_weight = float(response["query_result"]["boost"]["total_weight"]) / 1e6
+
+    return total_boost_weight
+
+
 # initial parameters
 luna_price, yluna_price, prism_price = get_prices()
 staked_luna = get_staked_luna()
 staking_yield = get_staking_yield(luna_price, staked_luna) * 100
 yluna_yield = (luna_price / yluna_price) * staking_yield
 
-# sidebar assumptions
-st.sidebar.header("Assumptions")
-st.sidebar.write("Exand the following sections to change your assumptions.")
+st.sidebar.header("User Inputs")
 
+# sidebar assumptions
+user_address = st.sidebar.text_input(
+    "Wallet Address", value="terra1376ghe2yntgxdct36chx83lrrva9jrrfym2j2p"
+)
+
+
+# asset prices
 with st.sidebar.expander("Asset Prices", expanded=True):
 
     luna_price_input = st.number_input(
@@ -115,44 +208,6 @@ with st.sidebar.expander("Asset Prices", expanded=True):
         format="%0.3f",
     )
 
-with st.sidebar.expander("PRISM Farm", expanded=True):
-
-    yluna_staked = (
-        st.number_input(
-            "Total yLUNA Staked (Million)",
-            min_value=0.0,
-            value=2.0,
-            step=0.5,
-            max_value=25.0,
-            format="%.1f",
-        )
-        * 1e6
-    )
-
-    num_users = st.number_input(
-        "Number of Users",
-        min_value=0,
-        value=3500,
-        step=100,
-        max_value=15000,
-    )
-
-with st.sidebar.expander("PRISM AMPS Vault", expanded=True):
-
-    avg_xprism = st.number_input(
-        "Avg xPRISM Pledged",
-        min_value=0,
-        value=10000,
-        step=1000,
-    )
-
-    avg_time = st.number_input(
-        "Avg xPRISM Pledge Duration (Days)",
-        min_value=0,
-        value=30,
-        step=1,
-        max_value=200,
-    )
 
 # updated parameters
 staking_yield = get_staking_yield(luna_price_input, staked_luna) * 100
@@ -186,115 +241,110 @@ col1.metric(label="PRISM Base Pool", value=f"{base_rewards:,.0f}")
 col2.metric(label="PRISM Boost Pool", value=f"{boost_rewards:,.0f}")
 
 # user input
-st.subheader("User's Position Input")
-st.markdown(
-    """
-    Input the number of yLUNA staked.  AMPS are calculated based on the amount of xPRISM pledged, and pledge duration.
-    """
-)
+st.subheader("User's Current Position")
 
+# protocol queries
+xprism_pledged = get_amps_vault_xprism()
+total_boost_weight = get_total_boost_weight()
+yluna_staked = get_yluna_staked()
+total_amps = total_boost_weight**2 / yluna_staked
+
+# user queries
+user_xprism, user_amps = get_user_amps(user_address)
+user_yluna, user_weight = get_user_prism_farm(user_address)
 
 col3, col4, col5 = st.columns(3)
 
-user_yluna = col3.number_input(
-    "yLUNA Staked",
-    min_value=1,
-    value=500,
-    step=100,
-)
+col3.metric(label="yLUNA Staked", value=f"{user_yluna:,.0f}")
+col4.metric(label="xPRISM Pledged", value=f"{user_xprism:,.0f}")
+col5.metric(label="AMPS Accrued", value=f"{user_amps:,.0f}")
 
-user_xprism = col4.number_input(
-    "xPRISM Pledged",
-    min_value=0,
-    value=500,
-    step=100,
-)
-
-user_amps_duration = col5.number_input(
-    "Pledge Duration (Days)", min_value=0, value=30, step=1, max_value=200
-)
-
-st.error("Unpledging any amount of xPRISM will reset all AMPS earned to zero.")
-
-
-st.subheader("AMPS Vault")
-st.markdown(
-    """
-    Each xPRISM accumulates 0.5 AMPS per day, up to a maximum of 100 AMPS.
-    
-    A user will reach their maximum aount of AMPS in roughly 200 days.
-    """
-)
-
-# AMPS calculations
-boost_per_xprism_per_hour = 0.021
-
-avg_boost = min(
-    avg_xprism * boost_per_xprism_per_hour * avg_time * 24, avg_xprism * 100
-)
-avg_yluna = yluna_staked / num_users
-avg_boost_weight = math.sqrt(yluna_staked / num_users * avg_boost)
-
-user_boost = min(
-    user_xprism * boost_per_xprism_per_hour * user_amps_duration * 24, user_xprism * 100
-)
-user_boost_weight = math.sqrt(user_yluna * user_boost)
-
-# PRISM Farm calculations
-user_base_rewards = base_rewards * user_yluna / yluna_staked
-user_boost_rewards = boost_rewards * user_boost_weight / (num_users * avg_boost_weight)
-combined_rewards = user_base_rewards + user_boost_rewards
-combined_apr = (combined_rewards * prism_price) / (user_yluna * yluna_price) * 100
-
-col12, col13 = st.columns(2)
-
-col12.metric("Average AMPS per User", f"{avg_boost:,.0f}")
-col13.metric("User's AMPS", f"{user_boost:,.0f}")
-
-# display output
-st.subheader("PRISM Farm Rewards")
-st.markdown(
-    "The $PRISM earned from the PRISM Farm vests and are claimable 30 days later."
-)
+# intial rewards
+base_rewards = 104_000_000 * user_yluna / yluna_staked
+base_apr = (base_rewards * prism_price) / (user_yluna * yluna_price) * 100
+boost_rewards = 26_000_000 * user_weight / total_boost_weight
+boost_apr = (boost_rewards * prism_price) / (user_yluna * yluna_price) * 100
+total_apr = base_apr + boost_apr
 
 col6, col7, col8 = st.columns(3)
 
-col6.metric(
-    label="Base PRISM Farmed",
-    value=f"{user_base_rewards:,.0f}",
-    delta=f"${user_base_rewards * prism_price:,.2f}",
-    delta_color="off",
+col6.metric(label="Base APR", value=f"{base_apr:,.2f}%")
+col7.metric(label="Boost APR", value=f"{boost_apr:,.2f}%")
+col8.metric(label="Total APR", value=f"{total_apr:,.2f}%")
+
+# user's new position
+st.sidebar.subheader("User's New Position")
+
+new_user_yluna = st.sidebar.number_input(
+    "yLUNA Staked",
+    min_value=0.0,
+    value=user_yluna,
+    step=user_yluna * 0.01,
+    format="%.0f",
 )
 
-col7.metric(
-    label="Boost PRISM Farmed",
-    value=f"{user_boost_rewards:,.0f}",
-    delta=f"${user_boost_rewards * prism_price:,.2f}",
-    delta_color="off",
+new_user_xprism = st.sidebar.number_input(
+    "xPRISM Pledged",
+    min_value=0.0,
+    value=user_xprism,
+    step=user_xprism * 0.01,
+    format="%.0f",
 )
 
-col8.metric(
-    label="Total PRISM Farmed",
-    value=f"{combined_rewards:,.0f}",
-    delta=f"${combined_rewards * prism_price:,.2f}",
-    delta_color="off",
+# reset user amps if they decrease
+if new_user_xprism < user_xprism:
+    user_amps = 0
+    st.sidebar.warning("Reducing xPRISM pledged will reset your accrued AMPS.")
+
+new_yluna_staked = yluna_staked + new_user_yluna - user_yluna
+
+# projected rewards over x days
+df = pd.DataFrame(range(1, 61), columns=["days"])
+
+# calculate APRs over time
+df["new_user_amps"] = new_user_xprism * 0.49992 * df["days"] + user_amps
+df["new_user_weight"] = (new_user_yluna * df["new_user_amps"]) ** (1 / 2)
+df["new_total_amps"] = (xprism_pledged + new_user_xprism) * 0.49992 * df[
+    "days"
+] + total_amps
+df["new_total_weight"] = (new_yluna_staked * df["new_total_amps"]) ** (1 / 2)
+
+df["new_base_rewards"] = 104_000_000 * new_user_yluna / yluna_staked
+df["new_base_apr"] = (
+    df["new_base_rewards"] * prism_price / (new_user_yluna * yluna_price) * 100
+)
+df["new_boost_rewards"] = 26_000_000 * df["new_user_weight"] / df["new_total_weight"]
+df["new_boost_apr"] = (
+    df["new_boost_rewards"] * prism_price / (new_user_yluna * yluna_price)
+) * 100
+df["new_total_apr"] = df["new_base_apr"] + df["new_boost_apr"]
+df["daily_rewards"] = (df["new_base_rewards"] + df["new_boost_rewards"]) / 365
+df["daily_rewards_usd"] = df["daily_rewards"] * prism_price
+df["rewards_sum"] = df["daily_rewards_usd"].cumsum()
+
+# plot APRs
+st.subheader("Projected APRs")
+st.write("User's projected APR over the next 60 days, given all else equal.")
+chart = px.line(
+    data_frame=df,
+    x="days",
+    y=["new_base_apr", "new_boost_apr", "new_total_apr"],
+    labels={"days": "Days", "value": "APR %", "variable": "Legend"},
+    template="plotly_dark",
 )
 
-# display output
-st.subheader("Luna and yLUNA Staking Yields")
-st.markdown(
-    """
-    APR comparison between regular Luna staking, yLUNA staking, and staking yLUNA in the PRISM Farm.
-    """
+# show chart
+st.plotly_chart(chart, use_container_width=True)
+
+st.subheader("Projected Rewards")
+st.write("User's cumulative rewards over the next 60 days, given all else equal.")
+
+chart = px.line(
+    data_frame=df,
+    x="days",
+    y=["daily_rewards_usd"],
+    labels={"days": "Days", "value": "Rewards (USD)", "variable": "Legend"},
+    template="plotly_dark",
 )
 
-col9, col10, col11 = st.columns(3)
-
-col9.metric(label="Luna Staking APR", value=f"{staking_yield:,.2f}%")
-
-col10.metric(label="yLUNA Staking APR", value=f"{yluna_yield:,.2f}%")
-
-col11.metric(label="PRISM Farm APR", value=f"{combined_apr:,.2f}%")
-
-# disclaimer
-st.warning("This tool was created for educational purposes only, not financial advice.")
+st.plotly_chart(chart, use_container_width=True)
